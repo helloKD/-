155132cbb77d6a7634c3fd78d8fe8d4442e5e6c3
chujinsong <chujinsong@qq.com>
2022-01-13 11:00:52 +0800

    面向对象基础01

---
+++ test/esm/a.js
@@ -0,0 +1,26 @@
+// let a = {
+//   name:'zs'
+// }
+// let b = 10;
+
+// let cc = class Fathter {
+//   constructor (name) {
+//     this.name = name
+//   }
+// }
+
+// export {a,b,cc}
+
+export class Father {
+  constructor (name) {
+    this.name = name
+  }
+}
+export class Son {
+  constructor (name) {
+    this.name = name
+  }
+}
+
+
+// export default 只能导出一个值
---
+++ test/esm/b.js
@@ -0,0 +1,15 @@
+// import {a as c,b,cc} from './a.js'
+
+
+// console.log(c,b,cc)
+
+// let ccs = new cc('lisi')
+// console.log(ccs);
+
+import {Father,Son} from "./a.js";
+
+let father = new Father('cjs')
+console.log(father);
+
+let son = new Son('cjs')
+console.log(son);
